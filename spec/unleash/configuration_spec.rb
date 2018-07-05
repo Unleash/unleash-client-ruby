d15696fc0f79451175f435c80052e3231cd78682
Renato Arruda <rarruda@rarruda.org>
2018-07-06 01:16:12 +0200

    Improved support for custom http headers

--- spec/unleash/configuration_spec.rb
+++ spec/unleash/configuration_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe Unleash do
 
   describe 'Configuration' do
     it "should have the correct defaults" do
-      config = Unleash::Configuration.new()
+      config = Unleash::Configuration.new
 
       expect(config.app_name).to be_nil
       expect(config.url).to be_nil
       expect(config.instance_id).to be_truthy
+      expect(config.custom_http_headers).to eq({})
       expect(config.disable_metrics).to be_falsey
 
       expect(config.refresh_interval).to eq(15)
@@ -17,8 +18,9 @@ RSpec.describe Unleash do
       expect(config.timeout).to eq(30)
       expect(config.retry_limit).to eq(1)
 
-      # expect(config.validate!).to raise_error(ArgumentError)
       expect(config.backup_file).to_not be_nil
+
+      expect{ config.validate! }.to raise_error(ArgumentError)
     end
 
     it "should by default be invalid" do
@@ -50,6 +52,45 @@ RSpec.describe Unleash do
       expect(config.client_register_url).to eq('https://testurl/api/client/register')
     end
 
-  end
+    it "should allow hashes for custom_http_headers via yield" do
+      Unleash.configure do |config|
+        config.url      = 'http://test-url/'
+        config.app_name = 'my-test-app'
+        config.custom_http_headers = {'X-API-KEY': '123'}
+      end
+      expect{ Unleash.configuration.validate! }.not_to raise_error
+      expect(Unleash.configuration.custom_http_headers).to eq({'X-API-KEY': '123'})
+    end
 
+    it "should allow hashes for custom_http_headers via new client" do
+      config = Unleash::Configuration.new(
+        url: 'https://testurl/api',
+        app_name: 'test-app',
+        custom_http_headers: {'X-API-KEY': '123'})
+
+      expect{ config.validate! }.not_to raise_error
+      expect(config.custom_http_headers).to eq({'X-API-KEY': '123'})
+    end
+
+    it "should not accept invalid custom_http_headers via yield" do
+      expect do
+        Unleash.configure do |config|
+          config.url      = 'http://test-url/'
+          config.app_name = 'my-test-app'
+          config.custom_http_headers = 123.456
+        end
+      end.to raise_error(ArgumentError)
+    end
+
+    # TODO: consider having consisten behaviour?
+    xit "should swallow silently invalid custom_http_headers if set via client" do
+      client = Unleash::Client.new(
+        url: 'https://testurl/api',
+        app_name: 'test-app',
+        custom_http_headers: 123.0)
+
+      expect(Unleash.configuration.custom_http_headers).to eq({})
+      expect{ Unleash.configuration.validate! }.not_to raise_error
+    end
+  end
 end

0cd721036f28937b3ddd5a05ccfec38754e7bd4e
Renato Arruda <rarruda@rarruda.org>
2018-11-08 11:06:50 +0100

    Bugfix: remoteAddress strategy did not work, as it used param with a mixed case key.

--- lib/unleash/strategy/remote_address.rb
+++ lib/unleash/strategy/remote_address.rb
@@ -5,13 +5,13 @@ module Unleash
         'remoteAddress'
       end
 
-      # need: params['ips'], context.remote_address
+      # need: params['IPs'], context.remote_address
       def is_enabled?(params = {}, context = nil)
-        return false unless params.is_a?(Hash) && params.has_key?('ips')
-        return false unless params.fetch('ips', nil).is_a? String
+        return false unless params.is_a?(Hash) && params.has_key?('IPs')
+        return false unless params.fetch('IPs', nil).is_a? String
         return false unless context.class.name == 'Unleash::Context'
 
-        params['ips'].split(',').map(&:strip).include?(context.remote_address)
+        params['IPs'].split(',').map(&:strip).include?(context.remote_address)
       end
     end
   end

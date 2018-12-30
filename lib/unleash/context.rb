e819dc165aa5ecd580a96083981edbb040db7580
Renato Arruda <rarruda@rarruda.org>
2018-12-30 16:26:10 +0100

    Keep Unleash::Context backwards compatible
    as well as ruby-style compliant.
    
    - Added unit tests to the Unleash::Context class.

--- lib/unleash/context.rb
+++ lib/unleash/context.rb
@@ -5,14 +5,33 @@ module Unleash
 
     def initialize(params = {})
       params_is_a_hash = params.is_a?(Hash)
-      self.user_id    = params_is_a_hash ? params.fetch('userId', '') : ''
-      self.session_id = params_is_a_hash ? params.fetch('sessionId', '') : ''
-      self.remote_address = params_is_a_hash ? params.fetch('remoteAddress', '') : ''
-      self.properties = params_is_a_hash && params[:properties].is_a?(Hash) ? params.fetch(:properties, {}) : {}
+      self.user_id    = fetch(params, 'userId')
+      self.session_id = fetch(params, 'sessionId')
+      self.remote_address = fetch(params, 'remoteAddress')
+      self.properties =
+        if params_is_a_hash && ( params.fetch(:properties, nil) || params.fetch('properties', nil) ).is_a?(Hash)
+          fetch(params, 'properties', {})
+        else
+          {}
+        end
     end
 
     def to_s
       "<Context: user_id=#{self.user_id},session_id=#{self.session_id},remote_address=#{self.remote_address},properties=#{self.properties}>"
+    end
+
+    private
+    # Fetch key from hash. Try first with using camelCase, and if not found, try with snake case.
+    # This way we are are idiomatically compliant with ruby, but still giving priority to the same
+    # key names as in the other clients.
+    def fetch(params, camelcase_key, default_ret = '')
+      return default_ret unless params.is_a?(Hash)
+      return params.fetch(camelcase_key, nil) || params.fetch(snakesym(camelcase_key), nil) || default_ret
+    end
+
+    # transform CamelCase to snake_case and make it a sym
+    def snakesym(key)
+      key.dup.gsub(/(.)([A-Z])/,'\1_\2').downcase.to_sym
     end
   end
 end
---
+++ spec/unleash/context_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+RSpec.describe Unleash::Context do
+  context 'parameters correctly assigned in initialization'
+  it "when using snake_case" do
+    params = {
+      user_id: '123',
+      session_id: 'verylongsesssionid',
+      remote_address: '127.0.0.2',
+      properties: {
+        fancy: 'polarbear'
+      }
+    }
+    context = Unleash::Context.new(params)
+    expect(context.user_id).to eq('123')
+    expect(context.session_id).to eq('verylongsesssionid')
+    expect(context.remote_address).to eq('127.0.0.2')
+    expect(context.properties).to eq({fancy: 'polarbear'})
+  end
+  it "when using camelCase" do
+    params = {
+      'userId' => '123',
+      'sessionId' => 'verylongsesssionid',
+      'remoteAddress' => '127.0.0.2',
+      'properties' => {
+        fancy: 'polarbear'
+      }
+    }
+    context = Unleash::Context.new(params)
+    expect(context.user_id).to eq('123')
+    expect(context.session_id).to eq('verylongsesssionid')
+    expect(context.remote_address).to eq('127.0.0.2')
+    expect(context.properties).to eq({fancy: 'polarbear'})
+  end
+  it "fails with non hash properties" do
+    params = {
+      'properties' => [1,2,3]
+    }
+    context = Unleash::Context.new(params)
+    expect(context.properties).to eq({})
+  end
+end

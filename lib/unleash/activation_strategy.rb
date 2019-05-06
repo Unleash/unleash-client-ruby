708fd32f5e209efea6d395dff8f9a9f602fafa27
Radosław Snarski <radeksnara@gmail.com>
2019-05-06 10:33:42 +0200

    Fix warning when params are nil in activation strategy

--- lib/unleash/activation_strategy.rb
+++ lib/unleash/activation_strategy.rb
@@ -1,11 +1,10 @@
-
-
 module Unleash
   class ActivationStrategy
     attr_accessor :name, :params
 
-    def initialize(name, params = {})
+    def initialize(name, params)
       self.name = name
+
       if params.is_a?(Hash)
         self.params = params
       else

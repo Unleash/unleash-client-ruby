d66482932d14f5cf08218b592a9e6a7d3f9c3059
Renato Arruda <rarruda@rarruda.org>
2021-05-10 21:43:34 +0200

    build/fix: use simplecov instead of the coveralls gems
    As coveralls doesn't seem to be well maintained anymore.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,24 @@
+require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config do |config|
+  config.report_with_single_file = true
+  config.single_report_path = 'coverage/lcov.info'
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::LcovFormatter
+])
+
+SimpleCov.start
+
 require "bundler/setup"
 require "unleash"
 require "unleash/client"
 
 require 'webmock/rspec'
 
-require 'coveralls'
-Coveralls.wear!
 
 WebMock.disable_net_connect!(allow_localhost: false)
 

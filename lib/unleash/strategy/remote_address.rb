0ec5cd8e4e4511dbe7fcd56602f26255f118e378
Renato Arruda <rarruda@rarruda.org>
2022-01-09 00:41:30 +0100

    use cleaner syntax when coercing strings into ipaddr or nil

--- lib/unleash/strategy/remote_address.rb
+++ lib/unleash/strategy/remote_address.rb
@@ -13,15 +13,23 @@ module Unleash
         return false unless params.fetch(PARAM, nil).is_a? String
         return false unless context.class.name == 'Unleash::Context'
 
-        remote_address = IPAddr.new(context.remote_address) rescue nil
+        remote_address = ipaddr_or_nil_from_str(context.remote_address)
 
         params[PARAM]
           .split(',')
           .map(&:strip)
-          .map{ |ipblock| IPAddr.new(ipblock) rescue nil }
+          .map{ |ipblock| ipaddr_or_nil_from_str(ipblock) }
           .compact
           .map{ |ipb| ipb.include? remote_address }
           .any?
+      end
+
+      private
+
+      def ipaddr_or_nil_from_str(ip)
+        IPAddr.new(ip)
+      rescue IPAddr::Error, StandardError
+        nil
       end
     end
   end

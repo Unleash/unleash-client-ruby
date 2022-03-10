a3d1bc66ecac504df6e1b60be25c5e1d768badef
sighphyre <liquidwicked64@gmail.com>
2022-03-10 12:48:47 +0200

    Update spec/unleash/constraint_spec.rb
    Co-authored-by: Renato Arruda <rarruda@rarruda.org>

--- spec/unleash/constraint_spec.rb
+++ spec/unleash/constraint_spec.rb
@@ -112,9 +112,6 @@ RSpec.describe Unleash::Constraint do
 
     it 'matches based on property STR_CONTAINS value' do
       context_params = {
-        user_id: '123',
-        session_id: 'verylongsesssionid',
-        remote_address: '127.0.0.1',
         properties: {
           env: 'development'
         }

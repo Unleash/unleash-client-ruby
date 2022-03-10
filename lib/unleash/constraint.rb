9ee1041021aec8e447f327a85da4c27d99e3528a
sighphyre <liquidwicked64@gmail.com>
2022-03-10 12:55:58 +0200

    Update lib/unleash/constraint.rb
    Co-authored-by: Renato Arruda <rarruda@rarruda.org>

--- lib/unleash/constraint.rb
+++ lib/unleash/constraint.rb
@@ -74,7 +74,7 @@ module Unleash
       yield(val1, val2)
     rescue ArgumentError
       Unleash.logger.warn "Unleash::ConstraintMatcher unable to parse either context_value (#{val1}) \
-      or constraint_value (#{val2}) into a version. This will always return false."
+      or constraint_value (#{val2}) into a version. Return false!"
       false
     end
 

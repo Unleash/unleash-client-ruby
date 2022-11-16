317b68232782a0d64a1cd7c3307bf269b2d1c84b
Renato Arruda <rarruda@rarruda.org>
2022-11-16 09:02:44 +0100

    fix: prevent race condition when manipulating metrics data (#122)
    The simplest way to guarantee correctness across threads is by syncronizing data access/change using mutexes,
    as it is done in the toggle fetcher.

--- lib/unleash/metrics.rb
+++ lib/unleash/metrics.rb
@@ -1,33 +1,40 @@
 module Unleash
   class Metrics
-    attr_accessor :features
-
-    # NOTE: no mutexes for features
+    attr_accessor :features, :features_lock
 
     def initialize
       self.features = {}
+      self.features_lock = Mutex.new
     end
 
     def to_s
-      self.features.to_json
+      self.features_lock.synchronize do
+        return self.features.to_json
+      end
     end
 
     def increment(feature, choice)
       raise "InvalidArgument choice must be :yes or :no" unless [:yes, :no].include? choice
 
-      self.features[feature] = { yes: 0, no: 0 } unless self.features.include? feature
-      self.features[feature][choice] += 1
+      self.features_lock.synchronize do
+        self.features[feature] = { yes: 0, no: 0 } unless self.features.include? feature
+        self.features[feature][choice] += 1
+      end
     end
 
     def increment_variant(feature, variant)
-      self.features[feature] = { yes: 0, no: 0 } unless self.features.include? feature
-      self.features[feature]['variant'] = {}     unless self.features[feature].include? 'variant'
-      self.features[feature]['variant'][variant] = 0 unless self.features[feature]['variant'].include? variant
-      self.features[feature]['variant'][variant] += 1
+      self.features_lock.synchronize do
+        self.features[feature] = { yes: 0, no: 0 } unless self.features.include? feature
+        self.features[feature]['variant'] = {}     unless self.features[feature].include? 'variant'
+        self.features[feature]['variant'][variant] = 0 unless self.features[feature]['variant'].include? variant
+        self.features[feature]['variant'][variant] += 1
+      end
     end
 
     def reset
-      self.features = {}
+      self.features_lock.synchronize do
+        self.features = {}
+      end
     end
   end
 end

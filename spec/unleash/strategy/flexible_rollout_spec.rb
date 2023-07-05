183685b936a1983291e68a963129337bc5526f0d
Gard Rimestad <gard@getunleash.ai>
2023-07-05 12:41:46 +0200

    normalize tests

--- spec/unleash/strategy/flexible_rollout_spec.rb
+++ spec/unleash/strategy/flexible_rollout_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Unleash::Strategy::FlexibleRollout do
     let(:strategy) { Unleash::Strategy::FlexibleRollout.new }
     let(:unleash_context) { Unleash::Context.new }
 
-    it 'should always be enabled when rollout is set to 100, disabled when set to 0' do
+    it 'should always be enabled when stickiness is default and rollout is set to 100' do
       params = {
         'groupId' => 'Demo',
         'rollout' => 100,
@@ -14,8 +14,39 @@ RSpec.describe Unleash::Strategy::FlexibleRollout do
 
       expect(strategy.is_enabled?(params, unleash_context)).to be_truthy
       expect(strategy.is_enabled?(params, nil)).to be_truthy
-      expect(strategy.is_enabled?(params.merge({ 'rollout' => 0 }), unleash_context)).to be_falsey
-      expect(strategy.is_enabled?(params.merge({ 'rollout' => 0 }), nil)).to be_falsey
+    end
+
+    it 'should always be disabled when stickiness is default and rollout is set to 0' do
+      params = {
+        'groupId' => 'Demo',
+        'rollout' => 0,
+        'stickiness' => 'default'
+      }
+
+      expect(strategy.is_enabled?(params, unleash_context)).to be_falsey
+      expect(strategy.is_enabled?(params, nil)).to be_falsey
+    end
+
+    it 'should always be enabled when stickiness is random and rollout is set to 100' do
+      params = {
+        'groupId' => 'Demo',
+        'rollout' => 100,
+        'stickiness' => 'random'
+      }
+
+      expect(strategy.is_enabled?(params, unleash_context)).to be_truthy
+      expect(strategy.is_enabled?(params, nil)).to be_truthy
+    end
+
+    it 'should always be disabled when stickiness is random and rollout is set to 0' do
+      params = {
+        'groupId' => 'Demo',
+        'rollout' => 0,
+        'stickiness' => 'random'
+      }
+
+      expect(strategy.is_enabled?(params, unleash_context)).to be_falsey
+      expect(strategy.is_enabled?(params, nil)).to be_falsey
     end
 
     it 'should behave predictably when based on the normalized_number' do
@@ -45,6 +76,7 @@ RSpec.describe Unleash::Strategy::FlexibleRollout do
       )
 
       expect(strategy.is_enabled?(params, custom_context)).to be_truthy
+      expect(strategy.is_enabled?(params, nil)).to be_falsey
     end
 
     it 'should be disabled when stickiness=customerId and customerId=63 and rollout=10' do
@@ -61,6 +93,7 @@ RSpec.describe Unleash::Strategy::FlexibleRollout do
       )
 
       expect(strategy.is_enabled?(params, custom_context)).to be_falsey
+      expect(strategy.is_enabled?(params, nil)).to be_falsey
     end
   end
 end

3b3c8438eb4a49fe1739fafe2ef5533c8dca06df
marocchino <marocchino@users.noreply.github.com>
2024-06-20 08:33:15 +0900

    fix: Unexpected TypeError on constraint

--- spec/unleash/constraint_spec.rb
+++ spec/unleash/constraint_spec.rb
@@ -333,6 +333,17 @@ RSpec.describe Unleash::Constraint do
 
       constraint = Unleash::Constraint.new('currentTime', 'DATE_AFTER', '2022-01-31T13:00:00.000Z')
       expect(constraint.matches_context?(context)).to be false
+
+      context_params = {
+        user_id: '123',
+        session_id: 'verylongsesssionid',
+        remote_address: '127.0.0.1',
+        current_time: DateTime.parse('2022-01-30T13:00:00.000Z')
+      }
+      context = Unleash::Context.new(context_params)
+
+      constraint = Unleash::Constraint.new('currentTime', 'DATE_AFTER', '2022-01-29T13:00:00.000Z')
+      expect(constraint.matches_context?(context)).to be true
     end
 
     it 'matches based on property DATE_BEFORE value' do
